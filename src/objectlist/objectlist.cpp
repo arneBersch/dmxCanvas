555e3a20fdb2c5a0958bc4d6a7862ab3a30ef5ed
arneBersch <97836617+arneBersch@users.noreply.github.com>
2024-12-26 14:48:56 +0100

    Create objectlist.cpp

---
+++ src/objectlist/objectlist.cpp
@@ -0,0 +1,192 @@
+#include "objectlist.h"
+
+ObjectList::ObjectList(QString softwareVersion) {
+    version = softwareVersion;
+    objects = QList<Object*>();
+}
+
+void ObjectList::newFile() {
+    removeRows(0, rowCount(), QModelIndex());
+    qDebug() << "Opened new file.";
+}
+
+void ObjectList::openFile(QString filename) {
+    QFile file(filename);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QMessageBox errorBox;
+        errorBox.setText("Can't open file.");
+        errorBox.exec();
+        return;
+    }
+    QXmlStreamReader fileStream(&file);
+    if ((fileStream.readNextStartElement()) && (fileStream.name().toString() == "Workspace")) {
+        while (fileStream.readNextStartElement()) {
+            if (fileStream.name().toString() == "Creator") {
+                while (fileStream.readNextStartElement()) {
+                    if ((fileStream.name().toString() == "Name") && (fileStream.readElementText() != "dmxCanvas")) {
+                        QMessageBox errorBox;
+                        errorBox.setText("This is not a dmxCanvas file.");
+                        errorBox.exec();
+                        return;
+                    } else if ((fileStream.name().toString() == "Version") && (fileStream.readElementText() != version)) {
+                        QMessageBox errorBox;
+                        errorBox.setText("This dmxCanvas version isn't compatible to the current version (" + version + ").");
+                        errorBox.exec();
+                        return;
+                    }
+                }
+            } else if(fileStream.name().toString() == "Objects") {
+                qDebug() << "<Objects>";
+                newFile();
+                while (fileStream.readNextStartElement()) {
+                    if (fileStream.name().toString() == "Object") {
+                        qDebug() << "<Object>";
+                        insertRows(rowCount(), 1);
+                        while (fileStream.readNextStartElement()) {
+                            if (fileStream.name().toString() == "Name") {
+                                qDebug() << "<Name>";
+                                setData(index((rowCount() - 1), ObjectListColumns::NameColumn), fileStream.readElementText());
+                            } else if (fileStream.name().toString() == "Channel") {
+                                qDebug() << "<Channel>";
+                                setData(index((rowCount() - 1), ObjectListColumns::ChannelColumn), fileStream.readElementText());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if (fileStream.hasError()) {
+        QMessageBox errorBox;
+        errorBox.setText("Can't open file because a XML passing error occured in line " + QString::number(fileStream.lineNumber()) + ": " + fileStream.errorString() + " (" + QString::number(fileStream.error()) + ")");
+        errorBox.exec();
+        return;
+    }
+    qDebug() << "Opened file " << filename;
+}
+
+void ObjectList::saveFile(QString filename) {
+    QFile file(filename);
+    if (!file.open(QIODevice::WriteOnly)) {
+        QMessageBox errorBox;
+        errorBox.setText("Unable to save file.");
+        errorBox.exec();
+        return;
+    }
+    QXmlStreamWriter fileStream(&file);
+    fileStream.setAutoFormatting(true);
+    fileStream.writeStartDocument();
+    fileStream.writeStartElement("Workspace");
+
+    fileStream.writeStartElement("Creator");
+    fileStream.writeTextElement("Name", "dmxCanvas");
+    fileStream.writeTextElement("Version", version);
+    fileStream.writeEndElement();
+
+    fileStream.writeStartElement("Objects");
+    for (Object* object : objects) {
+        fileStream.writeStartElement("Object");
+        fileStream.writeTextElement("Name", object->name);
+        fileStream.writeTextElement("Channel", QString::number(object->channel));
+        fileStream.writeEndElement();
+    }
+    fileStream.writeEndElement();
+
+    fileStream.writeEndElement();
+    fileStream.writeEndDocument();
+    qDebug() << "Saved file" << filename;
+}
+
+int ObjectList::rowCount(const QModelIndex &parent) const {
+    Q_UNUSED(parent);
+    return objects.length();
+}
+
+int ObjectList::columnCount(const QModelIndex &parent) const {
+    Q_UNUSED(parent);
+    return 2;
+}
+
+QVariant ObjectList::data(const QModelIndex &index, const int role) const {
+    if (!index.isValid() || (role != Qt::DisplayRole)) {
+        return QVariant();
+    }
+    const int row = index.row();
+    const int column = index.column();
+    if (row >= rowCount() || row < 0) {
+        return QVariant();
+    }
+    Object* object = objects[row];
+    if (column == ObjectListColumns::NameColumn) {
+        return object->name;
+    } else if (column == ObjectListColumns::ChannelColumn) {
+        return (QString::number(object->channel));
+    }
+    return QVariant();
+}
+
+bool ObjectList::setData(const QModelIndex &index, const QVariant &value, int role) {
+    if (index.isValid() && role == Qt::EditRole) {
+        const int row = index.row();
+        const int column = index.column();
+        Object* object = objects[row];
+        if (column == ObjectListColumns::NameColumn) {
+            object->name = value.toString();
+        } else if (column == ObjectListColumns::ChannelColumn) {
+            bool ok = false;
+            int channel = value.toString().toInt(&ok);
+            if (!ok || channel < 1 || channel > 506) {
+                return false;
+            }
+            object->channel = channel;
+        }
+        emit dataChanged(index, index, {Qt::DisplayRole, Qt::EditRole});
+        return true;
+    }
+    return false;
+}
+
+QVariant ObjectList::headerData(int section, Qt::Orientation orientation, int role) const {
+    if (role != Qt::DisplayRole) {
+        return QVariant();
+    }
+    if (orientation == Qt::Orientation::Horizontal) {
+        if (section == ObjectListColumns::NameColumn) {
+            return "Name";
+        } else if (section == ObjectListColumns::ChannelColumn) {
+            return "Channel";
+        }
+        return QVariant();
+    }
+    return QVariant();
+}
+
+Qt::ItemFlags ObjectList::flags(const QModelIndex &index) const {
+    if (!index.isValid()) {
+        return Qt::ItemIsEnabled;
+    }
+    return QAbstractItemModel::flags(index) | Qt::ItemIsEditable;
+}
+
+bool ObjectList::insertRows(int position, int rows, const QModelIndex &index) {
+    Q_UNUSED(index);
+    beginInsertRows(QModelIndex(), position, (position + rows - 1));
+    for (int row = 0; row < rows; row++) {
+        Object* object = new Object();
+        objects.insert(position, object);
+    }
+    endInsertRows();
+    return true;
+}
+
+bool ObjectList::removeRows(int position, int rows, const QModelIndex &index) {
+    Q_UNUSED(index);
+    beginRemoveRows(QModelIndex(), position, (position + rows - 1));
+    for (int row = 0; row < rows; row++) {
+        Object* object = objects[position];
+        objects.removeAt(position);
+        delete object;
+    }
+    endRemoveRows();
+    return true;
+}
