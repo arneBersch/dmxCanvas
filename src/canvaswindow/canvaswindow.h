9de2769edf3661c0c9202c9712b0e50e222964f9
arneBersch <97836617+arneBersch@users.noreply.github.com>
2024-12-26 14:46:56 +0100

    Add files via upload

---
+++ src/canvaswindow/canvaswindow.h
@@ -0,0 +1,27 @@
+/*
+    Copyright (c) Arne Bersch
+    This file is part of dmxCanvas.
+    dmxCanvas is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+    dmxCanvas is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along with dmxCanvas. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef CANVASWINDOW_H
+#define CANVASWINDOW_H
+
+#include <QtWidgets>
+
+#include "objectlist/objectlist.h"
+#include "server/sacnserver.h"
+
+class CanvasWindow : public QWidget {
+    Q_OBJECT
+public:
+    CanvasWindow(QWidget* parent, bool fullscreen, ObjectList* objectList, SacnServer* sacnServer);
+    void paintEvent(QPaintEvent *event) override;
+private:
+    ObjectList *objects;
+    SacnServer *sacn;
+};
+
+#endif // CANVASWINDOW_H
